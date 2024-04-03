417a279b6b59305466e9fe5c2c528659d3ee176a
= <delia.tapai@gmail.com>
2024-04-03 23:32:28 +0300

    commit 3

--- tema.cpp
+++ tema.cpp
@@ -1,6 +1,10 @@
+//Pentru ziua de nastere si luna voiam sa implementez ideea ca daca este ziua de nastere a clientului in data in care da comanda sa aiba o reducere de 10% la comanda
+
 #include <iostream>
 #include <string>
 #include <list>
+#include <vector>
+#include <unordered_map>
 
 class Client {
 private:
@@ -70,51 +74,45 @@ const std::string& Client::getNume() const {
 class Stoc {
 private:
     std::string nume;
-    double pret;
-    std::string produseIndisponibile;
+    static std::unordered_map<std::string, double> preturi;
 public:
+    Stoc(const std::string& nume = "") : nume(nume) {}
+
+    static void setPret(const std::string& nume, double pret) {
+        preturi[nume] = pret;
+    }
+
+    static double getPret(const std::string& nume) {
+        return preturi[nume];
+    }
+
     friend std::istream& operator>>(std::istream& is, Stoc& stoc) {
         std::cout << "Introduceti numele florii: ";
         is >> stoc.nume;
         return is;
     }
-    const double& getPret() const {
-        return pret;
+
+    const std::string& getNume() const {
+        return nume;
     }
 };
+
+std::unordered_map<std::string, double> Stoc::preturi;
 
 class Comanda {
 private:
     std::list<Stoc> flori;
-    double total_plata;
-
 public:
-    Comanda(std::list<Stoc> flori={}, double total_plata=0.0): flori(flori), total_plata(total_plata) {
-        this->flori = flori;
-        this->total_plata = total_plata;
-    }
-
-    Comanda(const Comanda &other) {
-        this->flori = other.flori;
-        this->total_plata = other.total_plata;
-    }
-
-    Comanda& operator=(const Comanda &other) {
-        if (this == &other)
-            return *this;
-        this->flori = other.flori;
-        this->total_plata = other.total_plata;
-        return *this;
-    }
+    Comanda(std::list<Stoc> flori = {}) : flori(flori) {}
 
     void ComandaFlori(const Stoc &floare) {
         flori.push_back(floare);
     }
 
-    double calculTotalPlata() {
+    double calculTotalPlata() const {
         double total = 0.0;
         for (const auto& floare : flori) {
-            total += floare.getPret();
+            total += Stoc::getPret(floare.getNume());
         }
         return total;
     }
@@ -124,19 +122,27 @@ public:
 };
 
 std::ostream& operator<<(std::ostream& os, const Comanda& comanda) {
-    os << "Numarul de flori cumparate este: " << comanda.flori.size() << std::endl;
+    os << "Numarul de flori cumparate este: " << comanda.flori.size();
+    return os;
 }
+
 std::istream& operator>>(std::istream& is, Comanda& comanda) {
     std::cout << "Introduceti numarul de flori pentru comanda: ";
     int numar_flori;
     is >> numar_flori;
     std::cout << "\n";
+
     if (numar_flori > 3) {
         std::string optiune;
         std::cout << "Doriti ca aceste flori sa fie facute in buchet? (Da/Nu): ";
         is >> optiune;
-        if (optiune=="Da") {
-            std::cout << "Buchet creat!" << std::endl;
+        if (optiune == "Da" || optiune == "da") {
+            std::cout << "Selectati flori pentru buchet:\n";
+            for (int i = 0; i < numar_flori; ++i) {
+                Stoc floare;
+                is >> floare;
+                comanda.ComandaFlori(floare);
+            }
             return is;
         }
     }
@@ -148,14 +154,24 @@ std::istream& operator>>(std::istream& is, Comanda& comanda) {
     }
     return is;
 }
+
 int main() {
+    std::cout<<"Preturile florilor disponibile: "<<"\n"<<"Trandafir: 10 lei"<<"\n"<<"Crin: 8 lei"<<"\n"<<"Lalea: 6 lei"<<"\n"<<"Bujor: 6 lei"<<"\n";
     Client c;
     std::cin >> c;
 
+    Stoc::setPret("trandafir", 10.0);
+    Stoc::setPret("crin", 8.0);
+    Stoc::setPret("lalea", 6.0);
+    Stoc::setPret("bujor", 6.0);
     Comanda comanda;
     std::cin >> comanda;
+    std::cout << comanda << "\n";
 
     std::cout << "Comanda plasata de " << c.getNume() << " este:\n" << comanda << std::endl;
+    double total_plata = comanda.calculTotalPlata();
+    std::cout << "Total de plata: " << total_plata << " lei\n";
 
     return 0;
 }
+
